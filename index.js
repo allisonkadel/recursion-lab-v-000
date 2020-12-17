c7921f777d6830c8b9345d526054a83a84de8c8a
Ally Kadel <akadel01@gmail.com>
2020-12-17 21:58:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
 
+const printString = (str) => {
+  console.log(str[0])
+  let substr = str.slice(1)
+  printString(substr)
+}
+
+// print out all the letters in a string
+
+printString('hello')
